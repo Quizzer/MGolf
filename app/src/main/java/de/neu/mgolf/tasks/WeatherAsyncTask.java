5e1477d46d98f2ce8370e5c0308e8a88ba506965
quizzer <hamburg2014>
2017-04-26 16:00:21 +0200

    start implementing WeatherAsyncTask

---
+++ app/src/main/java/de/neu/mgolf/tasks/WeatherAsyncTask.java
@@ -0,0 +1,11 @@
+package de.neu.mgolf.tasks;
+
+import android.os.AsyncTask;
+
+public class WeatherAsyncTask extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... params) {
+        return null;
+    }
+}
